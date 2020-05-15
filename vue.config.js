151f1e47092e7f2df87c347c34f173de29678fdb
WANNA <2457684290@qq.com>
2020-05-15 14:53:35 +0800

    '生产优化'

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,49 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
     publicPath: './',
     devServer: {
         port: 8888,
         open: true
     },
-    configureWebpack: {
-        plugins: [
+    configureWebpack: config => {
+        const plugins = [];
+        plugins.push(
             new CopyWebpackPlugin([
                 { from: path.join(__dirname, 'src/wx_api'), to: path.join(__dirname, 'dist/wx_api') },
                 { from: path.join(__dirname, 'src/assets/js/self-adaption.js'), to: path.join(__dirname, 'dist/self-adaption.js') }
             ])
+        )
+        if (process.env.NODE_ENV === 'production') {
+            plugins.push(
+                new CompressionWebpackPlugin({
+                    filename: '[path].gz[query]',
+                    algorithm: 'gzip',
+                    test: new RegExp('\\.(js|css)$'),
+                    threshold: 10240,
+                    minRatio: 0.8
+                })
+            )
+            // external配置
+            config.externals = {
+                'vue': 'Vue',
+                'vue-router': 'VueRouter'
+            }
+        }
+        config.plugins = [
+            ...config.plugins,
+            ...plugins
         ]
+    },
+    chainWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin('html')
+                .tap(args => {
+                    args[0].cdn = 'prod';
+                    return args;
+                });
+        }
     }
 }
